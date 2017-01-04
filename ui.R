c1af602bba353731c3c3ca90457ab184747edff0
mg1990 <mitesh.gupta19@gmail.com>
2017-01-04 17:57:00 +0530

    Create ui.R

---
+++ ui.R
@@ -0,0 +1,37 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+  
+  # Application title
+  titlePanel("Predict Horsepower from MPG"),
+  titlePanel("This app will helpful in finding the predicting the values of Horsepower for different Mile Per Gallon values"),
+  
+  # Sidebar with a slider input for number of bins 
+  sidebarLayout(
+    sidebarPanel(
+       sliderInput("sliderMPG","What is the MPG of car ?",10,35,value = 20),
+       checkboxInput("showModel1","Show/Hide Model 1",value = TRUE),
+       checkboxInput("showModel2","Show/Hide Model 2",value = TRUE),
+       submitButton("Submit") # New
+    ),
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+       plotOutput("plot1"),
+       h3("Predicted Horsepower from Model  1 :"),
+       textOutput("pred1"),
+       h3("Predicted Horsepower from Model  2 :"),
+       textOutput("pred2")
+    )
+  )
+))
